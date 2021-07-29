85ee14b45ca15927938bdc7bfa08b2ed4402852d
Денис Топычканов <lettenderman@gmail.com>
2021-07-29 19:14:12 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-$(document).ready(function () {
+$(document).ready(function () {   
+
     $(".header__burger").click(function (e) {
         $(".header").toggleClass('active');
     });
     $('.header__link').click(function (e) {
         if (!$('.header').hasClass('activ')){
             $(".header").toggleClass('active');     
-        }   
+        }
     });
 });
+
+ $(window).on('scroll', function () {
+    let windowScrollTop = $(window).scrollTop();
+    let seсtionList = $('section');
+    let lastSection = seсtionList[0];
+
+    for (const e of seсtionList) {
+        let offsetTop = $(e).offset().top;
+        if (windowScrollTop <= offsetTop - 88){
+            let id =$(lastSection).attr('id');
+            changeHeaderLink(id);
+            break;
+        }
+        if (e === seсtionList[seсtionList.length - 1]){
+            let id =$(e).attr('id');
+            changeHeaderLink(id);
+        }
+        lastSection = e;
+    }
+});
+
+function changeHeaderLink(id) {
+    $('.header__link.activ').removeClass('activ');
+    for (const link of $('.header__link')) {
+        if ($(link).attr('href') == "#" + id){
+            $(link).addClass('activ');
+            break;
+        }
+    }
+}
